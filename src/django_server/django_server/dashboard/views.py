d840073f3a668411074592f614aaa123b1030d6f
Ryan Gregson <ryangregson01@outlook.com>
2022-12-28 20:53:42 +0000

    Table created from sequence shown to user.
    JavaScript to dynamically create a table given the user input.
    Currently using random, but label is generated and the amino acids
    that are ordered/disordered have different background colours.

--- src/django_server/django_server/dashboard/views.py
+++ src/django_server/django_server/dashboard/views.py
@@ -10,6 +10,17 @@ def index(request):
         if form.is_valid():
             user_seq = form.save(commit=False)
             context_dict['input_seq'] = str(user_seq)
+            
+            # Make a random label just for the label test
+            import random
+            lab_arr = []
+            for i in range(len(str(user_seq))):
+                rand_val = random.randint(0, 1)
+                lab_arr.append(str(rand_val))
+
+            test_lab = ''.join(lab_arr)
+            context_dict['generated_label'] = test_lab
+
             return render(request, 'index.html', context=context_dict)
         else:
             print(form.errors)
