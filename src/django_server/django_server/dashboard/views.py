a08d5a082599c40a4434eda84f09707fe621e439
Ryan Gregson <ryangregson01@outlook.com>
2022-12-29 16:58:30 +0000

    Added trained model to server.
    From input sequence, model generates label.
    Creation of tensors and model are similar to google colab functions.
    Created a views_helper file for the functions of loading the model
    and creating tensors.

--- src/django_server/django_server/dashboard/views.py
+++ src/django_server/django_server/dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from dashboard.models import Protein
 from dashboard.forms import seqForm
+from dashboard.views_helper import *
+import torch
 
 def index(request):
     context_dict = {}
@@ -11,15 +13,16 @@ def index(request):
             user_seq = form.save(commit=False)
             context_dict['input_seq'] = str(user_seq)
             
-            # Make a random label just for the label test
-            import random
-            lab_arr = []
-            for i in range(len(str(user_seq))):
-                rand_val = random.randint(0, 1)
-                lab_arr.append(str(rand_val))
+            # user_seq is a string, so convert it to Tensor
+            full_seq = make_image(str(user_seq))
 
-            test_lab = ''.join(lab_arr)
-            context_dict['generated_label'] = test_lab
+            loaded_model = model_class()
+            with torch.no_grad():
+                predicted_label = loaded_model(full_seq)
+            predicted_label = torch.squeeze(predicted_label)
+            pred_lab_list = ['1' if (float(val) > 0.5) else '0' for val in predicted_label]
+            pred_lab_list = ''.join(pred_lab_list)
+            context_dict['generated_label'] = pred_lab_list
 
             return render(request, 'index.html', context=context_dict)
         else:
